3cbdc9af2517838bdbca4e29f99cf8946d80e37f
Penny <penny@penny.moe>
2024-03-18 17:23:19 -0400

    Refactor BasicMessage

--- SVCommon/BasicMessage.cs
+++ SVCommon/BasicMessage.cs
@@ -3,13 +3,13 @@
 namespace SVCommon;
 
 [MessagePackObject]
-public class MessageResponse
+public class BasicMessage
 {
     [Key(0)]
     public string Message { get; set; }
 
     public override string ToString()
     {
-        return $"MessageResponse - [Message: {Message}]";
+        return $"BasicMessage - [Message: {Message}]";
     }
 }
