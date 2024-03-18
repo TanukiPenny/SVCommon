138ec45f5f05a2824caadf98d7e2a153e6d6fcc3
Penny <penny@penny.moe>
2024-03-18 17:17:03 -0400

    Add BasicMessage

--- SVCommon/Class.cs
+++ SVCommon/Class.cs
@@ -1,5 +1,15 @@
-﻿namespace SVCommon;
+﻿using MessagePack;
 
-public class Class
+namespace SVCommon;
+
+[MessagePackObject]
+public class MessageResponse
 {
+    [Key(0)]
+    public string Message { get; set; }
+
+    public override string ToString()
+    {
+        return $"MessageResponse - [Message: {Message}]";
+    }
 }
