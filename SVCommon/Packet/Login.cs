ca70565a42a2e47a764123f98ab2702ee54ce7f1
Penny <penny@penny.moe>
2024-03-19 19:31:13 -0400

    Add login packet

---
+++ SVCommon/Packet/Login.cs
@@ -0,0 +1,15 @@
+using MessagePack;
+
+namespace SVCommon.Packet;
+
+[MessagePackObject]
+public class Login
+{
+    [Key(0)]
+    public string Nick { get; set; }
+    
+    public override string ToString()
+    {
+        return $"Login - [Nick: {Nick}]";
+    }
+}
